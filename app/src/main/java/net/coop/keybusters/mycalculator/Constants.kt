c0325a51c1b2ccb50805746f44dedf2642d1ac73
Sam_Bozman <sjbozman@gmail.com>
2018-02-21 12:12:13 -0800

    Setting up to read buttons from layout

---
+++ app/src/main/java/net/coop/keybusters/mycalculator/Constants.kt
@@ -0,0 +1,10 @@
+package net.coop.keybusters.mycalculator
+
+import android.widget.Button
+
+/**
+ * Created by SamB64 on 2/21/2018.
+ */
+class Constants {
+
+}
--- app/src/main/java/net/coop/keybusters/mycalculator/MainActivity.kt
+++ app/src/main/java/net/coop/keybusters/mycalculator/MainActivity.kt
@@ -2,17 +2,47 @@ package net.coop.keybusters.mycalculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(),View.OnClickListener  {
+
+    var buttons: Array<Button>? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_main)
+
+        buttons = arrayOfNulls(size = 10)
+
+        for (i in 0 until buttons.length) {
+            run {
+                val buttonID = "button" + (i + 1)
+
+                val resID = resources.getIdentifier(buttonID, "id", packageName)
+                buttons[i] = findViewById<View>(resID) as Button
+                buttons[i].setOnClickListener(this)
+            }
+        }
+
     }
 
-    val calc = Calculator(this)
 
+    override fun onClick(v: View) {
+        // TODO Auto-generated method stub
 
-val result = calc.double(2)
+        var index = 0
+        for (i in 0 until buttons.length) {
+            if (buttons[i].id === v.id) {
+                index = i
+                break
+            }
+        }
 
+        Toast.makeText(this, "Button clicked index => " + index, Toast.LENGTH_SHORT).show()
+    }
 }
