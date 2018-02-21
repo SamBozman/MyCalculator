3a7c41c911a99f4e898691aef3b246a86691ddb1
Sam_Bozman <sjbozman@gmail.com>
2018-02-21 14:10:38 -0800

    Set up onClick function (not yet working properly)

--- app/src/main/java/net/coop/keybusters/mycalculator/MainActivity.kt
+++ app/src/main/java/net/coop/keybusters/mycalculator/MainActivity.kt
@@ -1,48 +1,36 @@
 package net.coop.keybusters.mycalculator
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.view.View.OnClickListener
+
 import android.widget.Button
 import android.widget.Toast
 
 
-class MainActivity : AppCompatActivity(),View.OnClickListener  {
-
-    var buttons: Array<Button>? = null
-
+class MainActivity : AppCompatActivity()  {
+    private val TAG = "calc"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_main)
+    }
 
-        buttons = arrayOfNulls(size = 10)
-
-        for (i in 0 until buttons.length) {
-            run {
-                val buttonID = "button" + (i + 1)
-
-                val resID = resources.getIdentifier(buttonID, "id", packageName)
-                buttons[i] = findViewById<View>(resID) as Button
-                buttons[i].setOnClickListener(this)
-            }
-        }
+    // android:onClick="buttonHandler" in XML design file
+    fun buttonHandler(v: View) {
+        val i = v.id
+        showToastMessage("hello + $i",this)
 
     }
 
 
-    override fun onClick(v: View) {
-        // TODO Auto-generated method stub
-
-        var index = 0
-        for (i in 0 until buttons.length) {
-            if (buttons[i].id === v.id) {
-                index = i
-                break
-            }
-        }
-
-        Toast.makeText(this, "Button clicked index => " + index, Toast.LENGTH_SHORT).show()
+    fun showToastMessage(value: String, context: Context) {
+        Toast.makeText(applicationContext, value, Toast.LENGTH_SHORT).show()
     }
-}
+
+
+
+
+}//END MainActivity
